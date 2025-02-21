cf3c7cb2e00e7d9adf3fad9fc56b656f8c8f7ddb
TornikeAlugishvili <alugishvili.24@gmail.com>
2025-02-21 22:15:21 +0400

    commit

---
+++ day 01/homework/homework.py
@@ -0,0 +1,8 @@
+name = "tornike"
+name1 = "me"
+surname = "alugishvili."
+name2 = "var"
+name3 = "vswavlob_5_klasshi,"
+surname1 = "dainteresebuli_var_programirebit"
+surname2 = "2 welia."
+print ( name1 + "  " + name2 + " " + name + " " + surname + " " + name3+" " + surname1 + " " + surname2 )
